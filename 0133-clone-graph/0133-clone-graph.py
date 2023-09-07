ca44f114369639d315dcf991aa43fe696cd9310b
Bhargav Singapuri <bhsingapuri@gmail.com>
2023-09-07 11:57:39 +0800

    Time: 58 ms (10.99%), Space: 17 MB (23.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -38,4 +38,21 @@ class Solution:
                 if new_neighbor not in new.neighbors:
                     new.neighbors.append(new_neighbor)
         return store[1]
+
+# class Solution:
+#     def cloneGraph(self, node: "Node") -> "Node":
+#         oldToNew = {}
+
+#         def dfs(node):
+#             if node in oldToNew:
+#                 return oldToNew[node]
+
+#             copy = Node(node.val)
+#             oldToNew[node] = copy
+#             for nei in node.neighbors:
+#                 copy.neighbors.append(dfs(nei))
+#             return copy
+
+#         return dfs(node) if node else None
+
         
